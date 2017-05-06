f4a63d2005956adc2733e8176f5acdd1475b2adf
dach <daichi.yoshikawa1007@gmail.com>
2017-05-06 16:11:21 +0900

    新規ユーザー登録後にcookieにcurrent_userが登録されない問題を解決

--- db/schema.rb
+++ db/schema.rb
@@ -10,17 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170504140448) do
+ActiveRecord::Schema.define(version: 20170506065525) do
 
   create_table "messages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string   "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.integer  "room_id",    null: false
-    t.integer  "user_id_id"
+    t.integer  "room_id"
     t.integer  "user_id"
     t.index ["user_id"], name: "index_messages_on_user_id", using: :btree
-    t.index ["user_id_id"], name: "index_messages_on_user_id_id", using: :btree
   end
 
   create_table "room_users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
@@ -33,9 +31,9 @@ ActiveRecord::Schema.define(version: 20170504140448) do
   end
 
   create_table "rooms", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
-    t.string   "name",       default: "with friends"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string   "name",       null: false
   end
 
   create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
